8eff4ec07c733da0157fde36d0bf8db72c9bbb88
kelly6bf <kelly6bf@gmail.com>
2024-07-27 21:27:36 +0900

    config: #2 Swagger 설정 파일 생성

---
+++ src/main/java/site/kelly/memoria/common/infrastructure/swagger/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package site.kelly.memoria.common.infrastructure.swagger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo())
+                .components(securitySchemeComponents())
+                .addSecurityItem(securityRequirement());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Memoria API 문서")
+                .description("Memoria 프로젝트 API 문서")
+                .version("1.0.0");
+    }
+
+    private Components securitySchemeComponents() {
+        final SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("Authorization")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        return new Components().addSecuritySchemes("Authorization", bearerAuth);
+    }
+
+    private SecurityRequirement securityRequirement() {
+        return new SecurityRequirement().addList("Authorization");
+    }
+}
