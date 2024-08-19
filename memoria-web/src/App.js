5fd32c98bdddbe384e6286b0d003fa3530c04e9c
kelly6bf <kelly6bf@gmail.com>
2024-08-19 23:16:45 +0900

    chore: #1 감정 이미지 업로드 및 util 클래스 구현

--- memoria-web/src/App.js
+++ memoria-web/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {getEmotionImageById} from "./util/util";
 
 function App() {
   return (
     <div className="App">
       <h1>Memoria</h1>
+      <img alt="감정1" src={getEmotionImageById(1)} />
+      <img alt="감정1" src={getEmotionImageById(2)} />
+      <img alt="감정1" src={getEmotionImageById(3)} />
+      <img alt="감정1" src={getEmotionImageById(4)} />
+      <img alt="감정1" src={getEmotionImageById(5)} />
     </div>
   )
 }
