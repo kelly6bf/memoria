bd33ad084f1d746dcb75a525027b5ec5880b7bb3
kelly6bf <kelly6bf@gmail.com>
2024-08-31 13:01:57 +0900

    feat: #5 일기 상세 페이지 헤더 구현

--- memoria-web/src/pages/Diary.jsx
+++ memoria-web/src/pages/Diary.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import useDiary from "../hooks/useDiary";
+import {getFormattedDate} from "../util/util";
+import Header from "../components/Header/Header";
+import Button from "../components/Button/Button";
 
 export default function Diary() {
   const { diaryId } = useParams();
+  const data = useDiary(diaryId);
+
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+  const goEdit = () => {
+    navigate(`/diaries/${diaryId}/edit`);
+  }
+
+  if (!data) {
+    return <div>일기를 불러오고 있습니다... 🙇</div>
+  }
+
+  const {date, emotionId, content} = data;
+  const title = `${getFormattedDate(new Date(Number(date)))}`;
   return (
     <>
-      <h1>{diaryId}번 일기</h1>
-      <p>Diary Page입니다.</p>
+      <div>
+        <Header
+          title={title}
+          leftChild={<Button text={"< 뒤로 가기"} onClick={goBack} />}
+          rightChild={<Button text={"수정하기"} onClick={goEdit} />}
+        />
+      </div>
     </>
   );
 }
